fb0647a5e66246acd02e1d1ebd264159b0b01516
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-01-26 14:32:23 +0530

    Add files via upload

---
+++ data/Encroachment_6.js
@@ -0,0 +1,9 @@
+var json_Encroachment_6 = {
+"type": "FeatureCollection",
+"name": "Encroachment_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Location": "Somur", "Taluk": null }, "geometry": { "type": "Point", "coordinates": [ 78.153624151799505, 10.956578510810946 ] } },
+{ "type": "Feature", "properties": { "Location": "Koyamballi", "Taluk": null }, "geometry": { "type": "Point", "coordinates": [ 78.142102750403524, 10.959704707400798 ] } }
+]
+}
---
+++ data/Towns_9.js
@@ -0,0 +1,12 @@
+var json_Towns_9 = {
+"type": "FeatureCollection",
+"name": "Towns_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Chinna Dharapuram" }, "geometry": { "type": "Point", "coordinates": [ 77.851904970280913, 10.851908782267561, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Rajapuram" }, "geometry": { "type": "Point", "coordinates": [ 77.894901973406377, 10.80343187166493, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Karur" }, "geometry": { "type": "Point", "coordinates": [ 78.077691998377688, 10.95579676555972, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Mayilrangam" }, "geometry": { "type": "Point", "coordinates": [ 77.758427251001834, 10.86942950591148, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kalakurichi Nanjai" }, "geometry": { "type": "Point", "coordinates": [ 77.928091731398013, 10.85751613951955, -999999.0 ] } }
+]
+}
